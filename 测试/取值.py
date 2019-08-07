a5819f8d9d6e3139d88c8fec823e29ac064dceb5
ttp55 <1047697347@qq.com>
2019-08-07 16:33:20 +0800

    1

--- 测试/取值.py
+++ 测试/取值.py
@@ -1,21 +1,21 @@
 # @time:  2019/8/7 16:03
 # @author: WZG
 # encoding: utf-8
-
-a = {"status":0,
-     "msg":"success",
-     "data":[
+import jmespath
+a = {"status": 0,
+     "msg": "success",
+     "data": [
          {
-             "symbol":"xxx",
-             "a":"1998",
-             "on":"00000"
+             "symbol": "xxx",
+             "a": "1998",
+             "on": "00000"
          },
          {
-             "symbol":"yyy",
-             "a":"1997",
-             "on":"00000"
+             "symbol": "yyy",
+             "a": "1997",
+             "on": "00000"
          }
      ]}
 
-b = list(a["data"])
+b = jmespath.search("data[?symbol == 'yyy']", a)
 print(b)
