efc300ee5a2b43d380b915e8951798c51fbd1069
ttp55 <1047697347@qq.com>
2019-08-23 09:39:37 +0800

    1

--- CodeWars/23.py
+++ CodeWars/23.py
@@ -6,15 +6,13 @@
 def count_smileys(arr):
     x = 0
     for i in arr:
-        if ';' and 'D' in i:
-            x += 1
-        elif ':' and 'D' in i:
-            x += 1
-        elif ';' and ')' in i:
-            x += 1
-        elif ':' and ')' in i:
-            x += 1
+        if (';' and 'D' in i) or (':' and 'D' in i) or (';' and ')' in i) or (':' and ')' in i):
+            l = list(i)
+            if (l[0] == ';') or (l[0] == ':'):
+                x += 1
+            else:
+                return None
     return x
 
 
-print(count_smileys([':)',':(',':D',':O',':;']))
+print(count_smileys([':)', ':(', ':D', ':O', ':;']))
