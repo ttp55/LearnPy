6254c8f12a1a23c02c9ff2b3e2c48dd18926ad49
ttp55 <1047697347@qq.com>
2019-08-30 13:05:09 +0800

    1

--- CodeWars/23.py
+++ CodeWars/23.py
@@ -5,13 +5,15 @@
 
 def count_smileys(arr):
     x = 0
+    print(arr)
     for i in arr:
         if (';' and 'D' in i) or (':' and 'D' in i) or (';' and ')' in i) or (':' and ')' in i):
-            l = list(i)
-            if (l[0] == ';') or (l[0] == ':'):
-                x += 1
-            else:
-                return None
+            if i.count('o') == 0:
+                l = list(i)
+                if (l[0] == ';') or (l[0] == ':'):
+                    x += 1
+                else:
+                    return None
     return x
 
 
--- CodeWars/36.py
+++ CodeWars/36.py
@@ -1,36 +1,29 @@
 # @Time : 2019/8/30 9:06
 # @Author : WZG
 # --coding:utf-8--
-import math
 
 
 class PaginationHelper:
 
     def __init__(self, collection, items_per_page):
-        self.i = 0
-        self.list_l = []
         self.list_l1 = list(collection)
-        self.list_l2 = list(collection)
-        while self.i < math.ceil(len(self.list_l2)/items_per_page):
-            self.list_l.append(list(self.list_l1[:items_per_page]))
-            self.list_l1 = self.list_l1[items_per_page:]
-            self.i += 1
+        self.list_l2 = [self.list_l1[x : x + items_per_page] for x in range(0, len(self.list_l1), items_per_page)]
 
     def item_count(self):
-        return len(self.list_l2)
+        return len(self.list_l1)
 
     def page_count(self):
-        return len(self.list_l)
+        return len(self.list_l2)
 
     def page_item_count(self, page_index):
         try:
-            return len(self.list_l[page_index])
+            return len(self.list_l2[page_index])
         except:
             return -1
 
     def page_index(self, item_index):
         try:
-            r = map(lambda i: item_index in i, self.list_l)
+            r = map(lambda i: item_index + 1 in i, self.list_l2)
             return list(r).index(True)
         except:
             return -1
@@ -38,7 +31,7 @@ class PaginationHelper:
 
 coll = range(1, 25)
 helper = PaginationHelper(coll, 10)
-print(helper.item_count())
-print(helper.page_index(5))
-print(helper.page_item_count(2))
-print(helper.page_count())
+#print(helper.item_count())
+print(helper.page_index(0))
+#print(helper.page_item_count(2))
+#print(helper.page_count())
