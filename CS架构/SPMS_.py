3de9a5f7289b4fea7105e0d2390e51d045180b72
ttp55 <1047697347@qq.com>
2020-10-26 10:29:55 +0800

    窗口程序

---
+++ CS架构/SPMS_.py
@@ -0,0 +1,33 @@
+# @Time : 2020/10/13 15:18
+# @Author : WZG
+# --coding:utf-8--
+import autoit
+import time
+
+
+spmspath = "D:\SSSSSSSSPMS\Debug\SPMS.exe"
+
+autoit.run(spmspath)
+time.sleep(3)
+
+
+def jks_login():
+    autoit.send("{TAB}")
+    autoit.send('jks')
+    autoit.send("{DELETE 5}")
+    time.sleep(1)
+    autoit.send("{ENTER}")
+
+
+def admin_login():
+    autoit.send("{TAB}")
+    autoit.send('admin')
+    autoit.send("{DELETE 5}")
+    time.sleep(1)
+    autoit.send("{ENTER}")
+
+
+#jks_login()
+admin_login()
+
+
