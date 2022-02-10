90f879de4b072f3c2184651d7b18057267224a8d
ttp55 <1047697347@qq.com>
2022-02-10 16:25:41 +0800

    五子棋

--- wuziqi/wuziqiMain.py
+++ wuziqi/wuziqiMain.py
@@ -22,45 +22,67 @@ def whiteQ(x, y):
     pygame.draw.circle(screen, (255, 255, 255), (x, y), 18, 0)
 
 
-posXY = []
-while True:
-    highL = 40
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
+def wuzi():
+    posXY = []
+    pos_exist = []
+    color_Q = 0
+    while True:
+        highL = 40
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
 
-            pos_X = pos[0] // 40 * 40
-            pos_Y = pos[1] // 40 * 40
-            posXY.append(pos_X)
-            posXY.append(pos_Y)
-            if len(posXY) == 2:
-                print(pos_X, pos_Y)
-                blackQ(posXY[0], posXY[1])
-                posXY = []
+                if pos[0] % 40 > 20:
+                    pos_X = pos[0] // 40 * 40 + 40
+                    posXY.append(pos_X)
+                else:
+                    pos_X = pos[0] // 40 * 40
+                    posXY.append(pos_X)
+                if pos[1] % 40 > 20:
+                    pos_Y = pos[1] // 40 * 40 + 40
+                    posXY.append(pos_Y)
+                else:
+                    pos_Y = pos[1] // 40 * 40
+                    posXY.append(pos_Y)
 
-    blackQ(160, 160)
-    while highL <= 640:# 基本棋盘
-        pygame.draw.line(screen, (0, 0, 0), (40, highL), (640, highL), 1)
-        pygame.draw.line(screen, (0, 0, 0), (highL, 40), (highL, 640), 1)
+                if len(posXY) >= 2:
+                    if 700 > posXY[0] > 650 and 65 > posXY[1] > 40:
+                        wuzi()
+                    if posXY not in pos_exist and posXY[0] <= 640 and posXY[1] <= 640:
+                        pos_exist.append(posXY)
+                        if color_Q == 0:
+                            whiteQ(posXY[0], posXY[1])
+                            color_Q = 1
+                        else:
+                            blackQ(posXY[0], posXY[1])
+                            color_Q = 0
+                    posXY = []
 
-        highL += 40
-    # 棋盘加粗和黑点部分
+        clearS = pygame.image.load('cls.jpg')
+        screen.blit(clearS, [680, 40])
 
-    pygame.draw.line(screen, (0, 0, 0), (40, 200), (640, 200), 2)
-    pygame.draw.line(screen, (0, 0, 0), (40, 480), (640, 480), 2)
+        while highL <= 640:# 基本棋盘
+            pygame.draw.line(screen, (0, 0, 0), (40, highL), (640, highL), 1)
+            pygame.draw.line(screen, (0, 0, 0), (highL, 40), (highL, 640), 1)
 
-    pygame.draw.line(screen, (0, 0, 0), (200, 40), (200, 640), 2)
-    pygame.draw.line(screen, (0, 0, 0), (480, 40), (480, 640), 2)
-    pygame.draw.rect(screen, (0, 0, 0), ((197, 197), (8, 8)), 8)
-    pygame.draw.rect(screen, (0, 0, 0), ((197, 477), (8, 8)), 8)
-    pygame.draw.rect(screen, (0, 0, 0), ((477, 197), (8, 8)), 8)
-    pygame.draw.rect(screen, (0, 0, 0), ((477, 477), (8, 8)), 8)
+            highL += 40
+        # 棋盘加粗和黑点部分
+
+        pygame.draw.line(screen, (0, 0, 0), (40, 200), (640, 200), 2)
+        pygame.draw.line(screen, (0, 0, 0), (40, 480), (640, 480), 2)
+
+        pygame.draw.line(screen, (0, 0, 0), (200, 40), (200, 640), 2)
+        pygame.draw.line(screen, (0, 0, 0), (480, 40), (480, 640), 2)
+        pygame.draw.rect(screen, (0, 0, 0), ((197, 197), (8, 8)), 8)
+        pygame.draw.rect(screen, (0, 0, 0), ((197, 477), (8, 8)), 8)
+        pygame.draw.rect(screen, (0, 0, 0), ((477, 197), (8, 8)), 8)
+        pygame.draw.rect(screen, (0, 0, 0), ((477, 477), (8, 8)), 8)
 
 
-    pygame.display.flip() #更新全部显示
+        pygame.display.flip() #更新全部显示
 
-
-    #pygame.display.update()
+wuzi()
+        #pygame.display.update()
