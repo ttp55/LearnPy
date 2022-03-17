1e70cdc90444360cf88aab1f80090348fd7c07c0
ttp55 <1047697347@qq.com>
2022-03-17 16:00:37 +0800

    正则引用变量

--- 正则/zz2.py
+++ 正则/zz2.py
@@ -9,5 +9,6 @@ str1 = 'the little cat cat cat is in the hat hat hat hat, we like it.'
 # print(re.sub(r'(\w+)(\s\1)+', r'\1', str1))
 # print(re.findall(r'(\w+)(\s\1)+', str1))
 
-st = 'as1,2,3,4,5,cd1,2,3,cd'
-print(re.search(r'(\d,){5,}', st).group())
+st = 'as1,2,3,4,5,6,7,8,9,cd1,2,3,cd'
+m = max(re.findall(r'\d+', st))
+print(re.search(r'(\d,){%s,}' %m, st).group()) # 正则中引用变量
